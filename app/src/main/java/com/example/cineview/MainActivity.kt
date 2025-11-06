2269559e3be70237104206ed697024ffe7a20203
ST10468370 <ST10468370@vcconnect.edu.za>
2025-11-06 11:02:39 +0200

    Third Commit

--- app/src/main/java/com/example/cineview/MainActivity.kt
+++ app/src/main/java/com/example/cineview/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.cineview
 
 import android.content.Intent
+import android.content.pm.PackageItemInfo
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+private fun findViewById(id: Any) {
+}
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val packingList = mutableListOf<MovieDetails>()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,7 +39,41 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+        val editName = findViewById<EditText>(R.id.editName)
+        val editDirector = findViewById<EditText>(R.id.editDirector)
+        val editRating = findViewById<EditText>(R.id.editRating)
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val btnNext = findViewById<Button>(R.id.btnNext)
+        val btnAvg = findViewById<Button>(R.id.btnAvg)
+        val btnExit = findViewById<Button>(R.id.btnExit)
+
+
+        btnAdd.setOnClickListener {
+            val nameofmovie = editName.text.toString()
+            val nameofdirector = editDirector.text.toString()
+            val rating = editRating.text.toString().toIntOrNull() ?:0
+
+            if (nameofmovie.isNotBlank() && nameofdirector.isNotBlank() && rating >0){
+                packingList.add(MovieDetails(nameofmovie,nameofdirector, rating))
+                Toast.makeText(this,"Item added", Toast.LENGTH_SHORT).show()
+                editName.text.clear()
+                editDirector.text.clear()
+                editRating.text.clear()
+            } else{
+                Toast.makeText(this,"Please enter a valid name for a movie or director or rating", Toast.LENGTH_SHORT).show()
+            }
+        }
+        btnNext.setOnClickListener {
+            startActivity(Intent(this, SecondActivity2::class.java))
+        }
+
     }
 
 
-}
+
+
+    }
+
+
+
+
---
+++ app/src/main/java/com/example/cineview/MovieDetails.kt
@@ -0,0 +1,5 @@
+package com.example.cineview
+
+import android.R
+
+data class MovieDetails(val nameofmovie: String, val nameofdirector: String, val rating: Int)
